62f0f9b24b237a5e25d2bc3d2a54c34089c68ae6
Alia Mancisidor <alia.mancisidor@gmail.com>
2020-08-24 21:44:21 -0500

    pt 13

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]  
 }
